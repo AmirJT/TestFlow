b852668a0e5b3d683cb87b1c072096e9dc1539cf
AmirJT <ahjt28@gmail.com>
2025-03-05 20:13:38 -0800

    Fixed TypeScript issue in cleanDb.ts

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,19 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        const model = models[modelName];
+        if (!model || !model.db || !model.db.db) {
+            console.error(`Model "${modelName}" does not exist or is not properly initialized.`);
+            return;
+        }
+        let modelExists = await model.db.db.listCollections({ name: collectionName }).toArray();
+        if (modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        console.error("Error in cleanDb.ts:", err);
+        throw err;
+    }
+};
